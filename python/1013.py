1e06bb10e6e5d0ae1362834f5512b75d4c29bf53
José Nonato <josnonato161@gmail.com>
2024-11-01 16:00:08 -0300

    feat: 1013 to 1018

---
+++ python/1013.py
@@ -0,0 +1,3 @@
+num1, num2, num3 = list(map(int, input().split()))
+
+print(f'{max(num1, num2, num3)} eh o maior')
---
+++ python/1017.py
@@ -0,0 +1,6 @@
+horas = int(input())
+distancia = int(input())
+
+litros = (horas * distancia) / 12
+
+print(f'{litros:.3f}')
